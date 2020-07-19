a43a85be2c42d37222d39689d682ce0bf7086280
Gleb Bykovsky <gleb@bykovsky.dev>
2020-07-19 10:51:53 +0300

    feat: update angular core to actual version, set up moskup for project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { AnalyticsComponent } from './components/analytics/analytics.component';
+import { NotFoundComponent } from './components/shared/not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: '/employee', pathMatch: 'full' },
+  { path: 'employee', component: EmployeeListComponent },
+  { path: 'analytics', component: AnalyticsComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { AnalyticsComponent } from './components/analytics/analytics.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NotFoundComponent } from './components/shared/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    AnalyticsComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
